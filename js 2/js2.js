c6a44c87d06d45a1b175ddf1ee3afe22ef357314
sanjusudevan507@gmail.com <karshan3110@gmail.com>
2025-10-03 16:12:10 +0530

    Day 2

--- js 2/js2.js
+++ js 2/js2.js
@@ -1,7 +1,7 @@
 let name = prompt("Enter your name:");
 let a=true;
 while (a == true) {
-    let y = Number(prompt("Enter your age:"));
+    let y = Number(prompt("Enter your Age:"));
     if (y >= 18) {
         alert("Hello " + name + ", you are eligible to vote ");
     } else if (y < 18) {
